33c8f7f56a92064637a21f5d4b01a3be9eeb757a
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-07-02 13:07:00 -0400

    added values to fields in Rover class. Added a ReceiveMessage method that takes in a Message object and determines what to do with the message. It can change the rover's modes or change the rover's position. added an additional else if condition for the bonus mission in the event of an invalid command.

--- MarsRover/Rover.cs
+++ MarsRover/Rover.cs
@@ -9,6 +9,35 @@ namespace MarsRover
 
         public Rover(int position)
         {
+            Position = position;
+            Mode = "NORMAL";
+            GeneratorWatts = 110;
+        }
+
+        public void ReceiveMessage(Message message)
+        {
+            //Rover newRover = new Rover(Position);
+            //Console.WriteLine(newRover.ToString());
+            //newRover.ReceiveMessage(message);
+            for(int i = 0; i < message.Commands.Length; i++)
+            {
+                if(message.Commands[i].CommandType.Equals("MODE_CHANGE"))
+                {
+                    Mode = message.Commands[i].NewMode;
+                }
+                else if(message.Commands[i].CommandType.Equals("MOVE"))
+                {
+                    if(Mode != "LOW_POWER")
+                    {
+                        Position = message.Commands[i].NewPosition;
+                    }
+                }
+                else if(message.Commands[i].CommandType != "MOVE" || message.Commands[i].CommandType != "MODE_CHANGE")
+                {
+                    throw new ArgumentException("Invalid command.");
+                }
+            }
+            //Console.WriteLine(newRover.ToString());
         }
 
         public override string ToString()
--- MarsRoverTests/RoverTests.cs
+++ MarsRoverTests/RoverTests.cs
@@ -8,6 +8,76 @@ namespace MarsRoverTests
     [TestClass]
     public class RoverTests
     {
+        [TestMethod]
+        public void ConstructorSetsDefaultPosition()
+        {
+            Rover newRover = new Rover(98382);
+            Assert.AreEqual(newRover.Position, 98382);
+        }
+
+        [TestMethod]
+        public void ConstructorSetsDefaultMode()
+        {
+            Rover newRover = new Rover(98382);
+            Assert.AreEqual(newRover.Mode, "NORMAL");
+        }
+
+        [TestMethod]
+        public void ConstructorSetsDefaultGeneratorWatts()
+        {
+            Rover newRover = new Rover(98382);
+            Assert.AreEqual(newRover.GeneratorWatts, 110);
+        }
+
+        [TestMethod]
+        public void RespondsCorrectlyToModeChangeCommand()
+        {
+            Command[] commands = { new Command("MODE_CHANGE", "LOW_POWER") };
+            Message newMessage = new Message("Mode Change", commands);
+            Rover newRover = new Rover(98382);
+            newRover.ReceiveMessage(newMessage);
+            Assert.AreEqual("LOW_POWER", newRover.Mode); //expected, actual
+        }
+
+        [TestMethod]
+        public void DoesNotMoveInLowPower()
+        {
+            //Rover newRover = new Rover(98382);
+            Command[] commands = { new Command("MODE_CHANGE", "LOW_POWER"), new Command("MOVE", 100) };
+            Message newMessage = new Message("Mode Change", commands);
+            Rover newRover = new Rover(9001);
+            newRover.ReceiveMessage(newMessage);
+            Assert.AreEqual(9001, newRover.Position); //expected 9001, actual 9001. would not be able to
+            //move to 100 because of LOW_POWER
+        }
+
+        [TestMethod]
+        public void PositionChangesFromMoveCommand()
+        {
+            Command[] commands = { new Command("MOVE", 100) };
+            Message newMessage = new Message("Move", commands);
+            Rover newRover = new Rover(98382);
+            newRover.ReceiveMessage(newMessage);
+            Assert.AreEqual(100, newRover.Position); //expected 100, actual 100
+        }
+
+        [TestMethod]
+        public void RoverReturnsAMessageForAnUnknownCommand()
+        {
+            try
+            {
+                Command[] commands = { new Command("JUMP", 5) };
+                Message newMessage = new Message("Jump", commands);
+                Rover newRover = new Rover(19);
+                newRover.ReceiveMessage(newMessage);
+            }
+            catch (ArgumentException x)
+            {
+                Assert.AreEqual("Invalid command.", x.Message); //expected 100, actual 100
+            }
+        }
 
     }
+
+
 }
